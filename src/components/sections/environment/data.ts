3eedcd64cf0d99c631313c148407d3cd0a0ea220
AlitaNegrete <agnegretec@gmail.com>
2026-09-12 18:56:34 -0300

    Update data.ts

--- src/components/sections/environment/data.ts
+++ src/components/sections/environment/data.ts
@@ -1,19 +1,17 @@
 import img from "./assets/green-hands.png";
 
 export const environmentalImpactSection = {
-  title: "Environmental Impact",
-  subheading1_1: "Choosing [Company Name] for a",
-  subheading1_2: " Greener Tomorrow",
+  title: "Quiénes Somos",
+  subheading1_1: "Excelencia Multidisciplinaria",
+  subheading1_2: " al Servicio de su Comunidad",
   description:
-    "At [Company Name], we are committed to making a positive impact on the environment. By choosing our sustainable energy solutions, you contribute to:",
+    "En SN Consultores, creemos que la administración de comunidades exige un enfoque integral. Por ello, nuestro equipo combina décadas de experiencia en el ámbito legal, financiero y de recursos humanos para ofrecer un asesoramiento permanente y especializado.",
   impactPoints: [
-    "Reducing Carbon Emissions",
-    "Conserving Natural Resources",
-    "Preserving Biodiversity",
-    "Mitigating Climate Change",
-    "Promoting a Cleaner and Healthier Planet",
+    "Consultoría Legal Especializada: Abogado con 17 años de trayectoria profesional, especialista en la Ley de Copropiedad y Derecho Laboral, garantizando un respaldo jurídico sólido y actualizado.",
+    "Gestión Integral y Marco Jurídico: Contador Auditor con más de 14 años de experiencia. Especialista en auditorías para comunidades y administración de edificios, inscrita en el Registro Nacional de Administradores y complementada con formación jurídica.",
+    "Gestión de Capital Humano: Ingeniero en Gestión de Recursos Humanos con 17 años de experiencia, experto en control de nómina y liderazgo de grandes dotaciones, asegurando eficiencia y cumplimiento laboral.",
   ],
   visual: img,
-  callToAction: "Join Us in Building a Sustainable Future",
-  button: "Learn More",
+  callToAction: "Un equipo de alto nivel respaldando su patrimonio",
+  button: "Contáctanos",
 };
